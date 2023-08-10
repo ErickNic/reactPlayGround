0c2140182921988316581e844b40e72bde36171c
Erick <erickmartin.mezaalonso200245@gmail.com>
2023-08-10 12:04:55 -0600

    xd

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = (prop)=>{
+    return (
+        <div>
+            <h1>LOL</h1>
+            <p>COMO QUE NOOO</p>
+        </div>
+    )
+}
+
+export default App
